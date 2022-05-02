2211f014bf0f17c00b35452a090e4e6b92d90682
danmed <danmed@gmail.com>
2022-05-03 00:37:04 +0100

    Move source files
    Move the source files to MUXED folder to get them out of the way

--- automux.bat
+++ automux.bat
@@ -1,1 +1,5 @@
-for %%a in ("*.mp4") do ffmpeg -i "%%~na.mp4" -i "%%~na.srt" -c copy "%%~na.mkv"
+for %%a in ("*.mp4") do ( 
+	ffmpeg -i "%%~na.mp4" -i "%%~na.srt" -c copy "%%~na.mkv"
+	MOVE "%%~na.mp4" "F:\TORRENTS\COMPLETED\MUXED\%%~na.mp4"
+	MOVE "%%~na.srt" "F:\TORRENTS\COMPLETED\MUXED\%%~na.srt"
+)
