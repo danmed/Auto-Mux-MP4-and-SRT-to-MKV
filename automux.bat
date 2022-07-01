6047f3bd66f82340ef71775882370f0422f8f53d
danmed <danmed@gmail.com>
2022-07-01 22:23:38 +0100

    Update automux.bat
    Set Audio and SRT Language to English

--- automux.bat
+++ automux.bat
@@ -1,5 +1,5 @@
 for %%a in ("*.mp4") do ( 
-	ffmpeg -i "%%~na.mp4" -i "%%~na.srt" -c copy "%%~na.mkv"
+	ffmpeg -i "%%~na.mp4" -i "%%~na.srt" -metadata:s:s:0 title="English" -metadata:s:s:0 language="eng" -disposition:s:0 default -default_mode infer_no_subs -c copy "%%~na.mkv" 
 	MOVE "%%~na.mp4" "F:\TORRENTS\COMPLETED\MUXED\%%~na.mp4"
 	MOVE "%%~na.srt" "F:\TORRENTS\COMPLETED\MUXED\%%~na.srt"
 )
